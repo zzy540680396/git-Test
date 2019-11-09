33697a6f33e4cd130bc7304f54a591e19075a2fc
ZZY <540680396@qq.com>
2019-11-09 12:36:00 +0800

    22

--- git_test/src/main/java/com/zzy/User.java
+++ git_test/src/main/java/com/zzy/User.java
@@ -3,4 +3,29 @@ package com.zzy;
 public class User {
     private String name;
     private String id;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
